b68e4d6ea3d270089c7a1cfdecba8fcaace4417a
umer ejaz <107603379+Palladion44@users.noreply.github.com>
2023-03-11 21:11:04 +0500

    2222222222
    22

---
+++ src/admin/childrenTable.php
@@ -0,0 +1,289 @@
+<?php
+
+session_start();
+
+
+if(!isset($_SESSION['user'])){
+    header("location: login.php");
+}
+?>
+    
+
+<!DOCTYPE html> 
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard - SB Admin</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a name="logout" class="dropdown-item" href="index.php?logout=true">Logout</a></li>
+                       
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            
+                            <div class="sb-sidenav-menu-heading">Tables</div>
+                            <a class="nav-link" href="Index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Parents
+                            </a>
+                            <a class="nav-link" href="hospitalTable.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Hospitals
+                            </a>
+                            <a class="nav-link" href="childrenTable.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Children
+                            </a>
+                            <a class="nav-link" href="vaccinationsTable.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Vaccines
+                            </a>
+                            <a class="nav-link" href="requestsTable.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Requests
+                            </a>
+                            <a class="nav-link" href="bookingsTable.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Bookings
+                            </a>
+                            <a class="nav-link" href="vaccinationDatesTable.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                               Vaccination Dates
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                            <?php  echo $_SESSION['user']?>
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <!-- <h1 class="mt-4">Dashboard</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol> -->
+                        <!-- <div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">Primary Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-warning text-white mb-4">
+                                    <div class="card-body">Warning Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-success text-white mb-4">
+                                    <div class="card-body">Success Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-danger text-white mb-4">
+                                    <div class="card-body">Danger Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-6">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        <i class="fas fa-chart-area me-1"></i>
+                                        Area Chart Example
+                                    </div>
+                                    <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        <i class="fas fa-chart-bar me-1"></i>
+                                        Bar Chart Example
+                                    </div>
+                                    <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
+                                </div>
+                            </div>
+                        </div> -->
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table me-1"></i>
+                                DataTable Example
+                            </div>
+                            <div class="card-body">
+                              
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                        <th>Hospital_Id</th>
+                                            <th>HospitalName</th>
+                                            <th>HospitalEmail</th>
+                                            <th>HospitalPassword</th>
+                                            <th>HospitalAddress</th>
+                                            <th>Hepatitis_B_HepB</th>
+                                            <th>RotaVirus_RV</th>
+                                            <th>Diphteria_Tetanus_Pertussis_DTaP</th>
+                                            <th>H_Influenzae_type_B_Hib</th>
+                                            <th>Pneumococcal_PCV</th>
+                                            <th>Inactivated_Poliovirus_IPV</th>
+                                            <th>Measels_MumpsRubella_MMR</th>
+                                            <th>Varicella_VAR</th>
+                                            <th>Hepatitius_A_HepA</th>
+                                            <th>Influenza</th>
+                                            <th>Operations</th>
+                                            
+                                           
+                                        </tr>
+                                    </thead>
+                              
+                                    <tbody>
+    <h1>Children</h1>
+                                    
+                                        <?php
+                                        
+                                            include "config.php";
+                                        $res = mysqli_query($conn, "SELECT * FROM hospitals");
+                                        $row = mysqli_fetch_assoc($res);
+                                        ?>
+                                        <?php while($row = mysqli_fetch_assoc($res)){ ?>
+                                        
+                                        <tr>
+                                        <td><?php echo $row["hospital_id"]  ?></td>
+
+                                            <td><?php echo $row["hospitalname"]  ?></td>
+                                            <td> <?php echo $row["hospitalemail"]  ?></td>
+                                            <td><?php echo $row["Hospitalpassword"] ?></td>
+                                            <td><?php echo $row["hospitaladdress"] ?></td>
+                                            <td><?php echo $row["Hepatitis_B_HepB"] ?></td>
+                                            <td><?php echo $row["RotaVirus_RV"] ?></td>
+                                            <td><?php echo $row["Diphteria_Tetanus_Pertussis_DTaP"] ?></td>
+
+                                            <td><?php echo $row["H_Influenzae_type_B_Hib"] ?></td>
+
+                                            <td><?php echo $row["Pneumococcal_PCV"] ?></td>
+
+                                            <td><?php echo $row["Inactivated_Poliovirus_IPV"] ?></td>
+
+                                            <td><?php echo $row["Measels_MumpsRubella_MMR"] ?></td>
+                                            <td><?php echo $row["Varicella_VAR"] ?></td>
+
+                                            <td><?php echo $row["Hepatitius_A_HepA"] ?></td>
+
+                                            <td><?php echo $row["Influenza"] ?></td>
+
+                                            
+
+                                            
+
+                                            
+                                            <td><button class="btn btn-primary">
+                                    <a href="update.php?hospitalid=<?php echo $row["hospital_id"] ?>" class="text-white "> Update
+                                    </a></button>
+                                <button class="btn btn-danger">
+                                    <a href="delete.php?hospitalid=<?php echo $row["hospital_id"] ?>" class="text-white "> Delete</a>
+                                </button></td>
+                            
+                                           
+                                        </tr>
+                                       
+                                   <?php } ?>
+                                    </tbody>
+
+
+                                    
+                                </table>
+
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright Your Website 2022</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+</html>
+<?php
+ if (isset($_GET["logout"])) {
+    session_unset();
+    session_destroy();
+
+    header("location:login.php");
+    echo '<script>window.location.href = "login.php"</script>'; 
+
+    
+}
+
+?>
