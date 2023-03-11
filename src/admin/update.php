251ff8f3fe08781da0461a11912ace2271a96b39
umer ejaz <107603379+Palladion44@users.noreply.github.com>
2023-03-11 20:52:05 +0500

    3
    3

--- src/admin/update.php
+++ src/admin/update.php
@@ -43,7 +43,7 @@ if (isset($_GET['parentid'])) {
 
         <body>
             <div class="container  mt-3">
-                <h2 class="text-center">Update Your Information</h2>
+                <h2 class="text-center">Update Parent Information</h2>
                 <form action="update.php" method="post">
                 <div class="mb-3 mt-3">
                         <label for="email">Parent Name</label>
@@ -155,27 +155,27 @@ if (isset($_GET['hospitalid'])) {
 
         <body>
             <div class="container  mt-3">
-                <h2 class="text-center">Update Your Information</h2>
+                <h2 class="text-center">Update Hospital Information</h2>
                 <form action="update.php" method="post">
                 <div class="mb-3 mt-3">
-                        <label for="email">Hospital id Name</label>
-                        <input type="text"  class="form-control" id="email" name="pid" value="<?php echo $pid ?>">
+                        <label for="email">Hospital id </label>
+                        <input type="text"  class="form-control" id="email" name="hid" value="<?php echo $row['hospital_id'] ?>">
                     </div>
                     <div class="mb-3 mt-3">
-                        <label for="email">Parent Name</label>
-                        <input type="text" class="form-control" id="email" name="pname" value="<?php echo $pname ?>">
+                        <label for="email">Hospital Name</label>
+                        <input type="text" class="form-control" id="email" name="hname" value="<?php echo $row['hospitalname'] ?>">
                     </div>
                     <div class="mb-3 mt-3">
-                        <label for="email">Parent Email</label>
-                        <input type="text" class="form-control" id="email" name="pemail" value="<?php echo $pemail ?>">
+                        <label for="email">Hospital Email</label>
+                        <input type="text" class="form-control" id="email" name="hemail" value="<?php echo $row['hospitalemail']; ?>">
                     </div>
                     <div class="mb-3 mt-3">
-                        <label for="email">Parent Password</label>
-                        <input type="text" class="form-control" id="email" value="<?php echo $ppassword ?>" name="ppassword">
+                        <label for="email">Hospital Password</label>
+                        <input type="text" class="form-control" id="email" value="<?php echo $row['Hospitalpassword'] ?>" name="hpassword">
                     </div>
 
                 
-                    <button type="submit" class="btn btn-primary mt-2" name="updateparent">Update</button>
+                    <button type="submit" class="btn btn-primary mt-2" name="updatehospital">Update</button>
                 </form>
                 
         </body>
@@ -186,13 +186,13 @@ if (isset($_GET['hospitalid'])) {
 
                 <?php
 
-                if (isset($_POST['updateparent'])) {
-                    $pid = $_POST['pid'];
-                    $pname =  $_POST['pname'];
-                    $pemail =  $_POST['pemail'];
-                    $ppassword =  sha1($_POST['ppassword']);
-                    $update = "UPDATE `parents` SET
-                     `parentname` = '$pname', `parentemail` = '$pemail', `parentpassword` = '$ppassword' WHERE  `parent_id` = '$pid'";
+                if (isset($_POST['updatehospital'])) {
+                    $hid = $_POST['hid'];
+                    $hname =  $_POST['hname'];
+                    $hemail =  $_POST['hemail'];
+                    $hpassword =  sha1($_POST['hpassword']);
+                    $update = "UPDATE `hospitals` SET
+                     `hospitalname` = '$hname', `hospitalemail` = '$hemail', `hospitalPassword` = '$hpassword' WHERE  `hospital_id` = '$hid'";
        
 
                     $updateRes = mysqli_query($conn, $update);
@@ -202,7 +202,7 @@ if (isset($_GET['hospitalid'])) {
 
 
                         echo "record updated";
-                        header("location: index.php");
+                        header("location: hospitalTable.php");
 
                     } else {
 
