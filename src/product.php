f576b8bb97fa6aa1c06ab3faf1de986b6a9127ef
Aiman Hussain <108855291+AimanHussainWebTech@users.noreply.github.com>
2023-03-20 16:45:39 +0500

    9 se 5 bajhe thak ye kia he kaam

--- src/product.php
+++ src/product.php
@@ -53,113 +53,120 @@
 <section id="banner">
         <img src="vaccinelogo.jpg" class="logo">
 </section>
-<section id="cards">
+<div class="container">
 <div class="title-text">
-        <h1>search Vaccine</h1> 
+        <h1>AVAILABLE VACCINE</h1> 
 </div>
 	<div  class="cards1">
 		<div class="row gy-3 my-3">
-			<div class=" col-sm-6 col-md-4 col-lg-3">
+			<div class=" col-4">
 			<div class="card" >
   <img src="./proimages/hapatits.jpg" class="card-img-top" alt="image">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">HAPATHSIS B(Hepb)</h5>
+    <p class="card-text">Hepatitis B vaccines contain one of the proteins from the surface of
+       the hepatitis B virus (hep B surface antigen, or HBsAg). This protein is made by
+       inserting the genetic code into yeast cells, which removes any risk of viral DNA 
+       getting into the final product. This process is called recombinant DNA technology</p>
+    
 </div>
 </div>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3">
+<div class="col-4 col-sm-4">
 <div class="card" >
-  <img src="./proimages/tdap.jpg" class="card-img-top" alt="image">
+  <img src="./proimages/rotav.jpg" class="card-img-top" alt="image">
+  <div class="card-body">
+    <h5 class="card-title">Rotavirus (RV)</h5>
+    <p class="card-text">Rotavirus vaccine is the best way to protect your child against rotavirus disease. Most children (about 9 out of 10)
+       who get the vaccine will be protected from severe rotavirus disease</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+</div>
+</div>
+</div>
+<div class="col-4  col-sm-4">
+<div class="card" >
+  <img src="./proimages/diphtheria.jpg" height="190px" class="card-img-top" alt="image">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-text">The DTaP vaccine protects young children from diphtheria, tetanus, and whooping cough. 
+      The DT vaccine protects young children from diphtheria and tetanus. 
+      The Tdap vaccine protects preteens, teens, and adults from tetanus, diphtheria, 
+      and whooping cough</p>
 </div>
 </div>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3">
-<div class="card" >
-  <img src="./proimages/Tetanus vaccine.jpg" class="card-img-top" alt="image">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-</div>
-</div>
-</div>
-<div class="col-sm-6 col-md-4 col-lg-3">
+<div class="col-4  col-sm-4">
 <div class="card" >
   <img src="./proimages/infulenza.jpg" class="card-img-top" alt="image">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-text"> Influenza vaccines are vaccines that protect against the four
+       influenza viruses that research indicates will be most common during the upcoming 
+       season. </p>
+    
 </div>
 </div>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3">
+<div class="col-4 col-sm-4">
 <div class="card" >
-  <img src="./proimages/rotav.jpg" class="card-img-top" alt="image">
+  <img src="./proimages/pnemo.jpg" class="card-img-top" alt="image">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-text">The pneumococcal vaccine protects against serious and potentially fatal pneumococcal
+       infections. It's also known as the pneumonia vaccine. </p>
+    
 </div>
 </div>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3">
+<div class="col-4 col-sm-4">
+<div class="card" >
+  <img src="./proimages/polio.jpg" class="card-img-top" alt="image">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">CDC recommends that all children get polio vaccine to protect 
+      against polio, or poliomyelitis, as part of the series of routine childhood vaccines. </p>
+</div>
+</div>
+</div>
+<div class="col-4 col-sm-4">
 <div class="card" >
   <img src="./proimages/measle mumpus.jpg" class="card-img-top" alt="image">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-text">The vaccine protects against three diseases: measles, mumps, and
+       rubella. CDC recommends children get two doses of MMR vaccine, starting with the 
+       first dose at 12 through 15 months of age, and the second dose at 4 through 6 years
+        of age.Teens and adults should also be up to date on their MMR vaccination</P>
+   
 </div>
 </div>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3">
-<div class="card" >
-  <img src="./proimages/rotav.jpg" class="card-img-top" alt="image">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-</div>
-</div>
-</div>
-<div class="col-sm-6 col-md-4 col-lg-3">
-<div class="card" >
-  <img src="./proimages/hepathsis1.jpg" class="card-img-top" alt="image">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-</div>
-</div>
-</div>
-<div class="col-sm-6 col-md-4 col-lg-3">
+<div class="col-4 col-sm-4">
 <div class="card" >
   <img src="./proimages/varicella_vaccine.jpg" class="card-img-top" alt="image">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-text">The varicella vaccine protects against chickenpox (varicella),
+       a common and very contagious childhood viral illness. It also protects against 
+       shingles</p>
+    
 </div>
 </div>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3">
+<div class="col-4 col-sm-4">
 <div class="card" >
-  <img src="./proimages/pnemo.jpg" class="card-img-bottom" alt="image">
+  <img src="./proimages/ha.jpg" class="card-img-top" alt="image">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-text">Hepatitis A vaccine can prevent hepatitis A.
+       Hepatitis A is a serious liver disease. It is usually spread through close, 
+       personal contact with an infected person or when a person unknowingly ingests the 
+       virus from objects, food, or drinks</P>
+    
 </div>
 </div>
 </div>
-</section>
+<!-- <link  src=https://www.livingstonhealthcare.org/images/childImmunizationMagnet.jpg > -->
 
 <script>
     var menuBtn = document.getElementById("menuBtn")
